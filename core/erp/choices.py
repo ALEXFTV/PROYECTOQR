528fdc23b2ba15c07337fea9e772403e97e9e9e6
alexftvdj <alexander.tipan1215@utc.edu.ec>
2021-03-02 22:18:16 -0500

    subiendo todo el proyecto

---
+++ core/erp/choices.py
@@ -0,0 +1,12 @@
+gender_choices = (
+    ('male','Masculino'),
+    ('female','Femenino'),
+)
+gender_choices_mascot = (
+    ('male','MACHO'),
+    ('female','HEMBRA'),
+)
+gender_especie = (
+    ('PERRO','PERRO'),
+    ('GATO','GATO'),
+)
